778e3ad1fbcdd36f9bb5b456da8fb4a8f4859266
noelleter <shdmsrla@gmail.com>
2019-09-16 10:18:06 +0900

    Problem 4

--- OOD03/190916_LAB04.cpp
+++ OOD03/190916_LAB04.cpp
@@ -1,9 +1,62 @@
 //
-//  190916_LAB04.cpp
+//  190916_LAB03.cpp
 //  OOD03
 //
 //  Created by 김노은 on 16/09/2019.
 //  Copyright © 2019 Noeun-Kim. All rights reserved.
 //
 
-#include <stdio.h>
+
+#include <iostream>
+#define MAXSTACKSIZE 1000
+
+class Stack{
+    int top;
+  public:
+    int a[MAXSTACKSIZE];
+    Stack(){ top = -1; }
+    bool push(int x);
+    int pop();
+    
+};
+
+bool Stack::push(int x){
+    if(top >= (MAXSTACKSIZE - 1)){
+        std::cout << "오류: 스택이 넘쳤습니다." << std::endl;
+        return false;
+    }
+    else{
+        top++;
+        a[top] = x;
+        std::cout << x << "이 스택에 들어갔습니다." << std::endl;
+        return true;
+    }
+}
+
+int Stack::pop(){
+    if(top < 0){
+        std::cout << "오류: 스택이 비었습니다." << std::endl;
+        return 0;
+    }
+    else{
+        int x = 0;
+        x = a[top];
+        top--;
+        return x;
+    }
+}
+
+int main(){
+    class Stack s;
+    s.push(7);
+    s.push(88);
+    s.push(999);
+    
+    std::cout << s.pop() << " 을 스택에서 꺼냈습니다." << std::endl;
+    std::cout << s.pop() << " 을 스택에서 꺼냈습니다." << std::endl;
+    std::cout << s.pop() << " 을 스택에서 꺼냈습니다." << std::endl;
+    
+    system("read");
+    
+    return 0;
+}
