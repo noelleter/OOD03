5b3de7b15945fb10c825927fdac324bb49f7ee6a
noelleter <shdmsrla@gmail.com>
2019-09-16 09:08:11 +0900

    Initial Commit

---
+++ OOD03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOD03
+//
+//  Created by 김노은 on 16/09/2019.
+//  Copyright © 2019 Noeun-Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
